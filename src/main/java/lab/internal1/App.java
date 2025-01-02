4b446b72427d9a83fbbeea2882b1670c5860c3dc
Gayatri <gayatridevi@gmail.com>
2025-01-02 06:47:36 +0530

    commit-1

---
+++ src/main/java/lab/internal1/App.java
@@ -0,0 +1,13 @@
+package lab.internal1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
